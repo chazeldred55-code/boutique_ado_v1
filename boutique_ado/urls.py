e745bfb6a496688edee0d61df0ef71634d9f5bd4
Coolafdood <chazeldred55@gmail.com>
2026-01-27 11:31:14 +0000

    Add checkout view, URL, and template scaffold

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+
 
 
 urlpatterns = [
@@ -25,6 +27,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('bag/', include('bag.urls')),
     path("admin/", admin.site.urls),
+    path("checkout/", include("checkout.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
